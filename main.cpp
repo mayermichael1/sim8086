95650417b281a84349e6e12ce322254689273170
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-29 21:16:41 +0200

    read binary file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <unistd.h>
 
-int main(void){
-    std::cout << "Hello sim8086" << std::endl;
-    return 0;
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+    {
+      return 1;
+    }
+
+  char* filename = argv[1];
+  FILE *fp = fopen(filename, "rb");
+
+  if ( fp == NULL)
+    {
+      return 1;
+    }
+
+  char nextInstruction[2];
+  while (fread(nextInstruction, 1, 2, fp)) 
+    {
+      printf("%i %i ", nextInstruction[0], nextInstruction[1]);
+      
+    }
+
+  printf("\n");
+
+  fclose(fp);
+  return 0;
 }
