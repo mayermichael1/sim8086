3af42b2b147a2c03f88bc6acb76007c0a9449d5f
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-29 22:22:21 +0200

    decimal to binary string

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
+
+typedef char unsigned byte;
+
+static const byte MOV_OPERATION = 0b10001000;
+//static const byte W_MASK = 0b00000001;
+
+char* 
+byte_to_binary_string(byte input)
+{
+  char* binary_string = (char*)malloc(sizeof(input)) + 1;
+  byte mask = (byte)(1 << ((sizeof(input) * 8) - 1));
+  unsigned int i = 0;
+
+  for( i = 0 ; i < sizeof(input) * 8; i++)
+    {
+      if( input & mask )
+        {
+          binary_string[i] = '1';
+        }
+      else
+        {
+          binary_string[i] = '0';
+        }
+      mask = mask >> 1;
+    }
+  binary_string[i] = 0;
+
+  return binary_string;
+}
 
 int main(int argc, char** argv)
 {
@@ -16,15 +46,26 @@ int main(int argc, char** argv)
       return 1;
     }
 
-  char nextInstruction[2];
-  while (fread(nextInstruction, 1, 2, fp)) 
+  byte next_instruction[2];
+  char operation[4] = "";
+  //char register_1[4] = "";
+  //char register_2[4] = "";
+  while (fread(next_instruction, sizeof(byte), sizeof(next_instruction), fp)) 
     {
-      printf("%i %i ", nextInstruction[0], nextInstruction[1]);
-      
+      printf("mc: %s %s ", 
+        byte_to_binary_string(next_instruction[0]), 
+        byte_to_binary_string(next_instruction[1]));
+
+      if ((next_instruction[0] & MOV_OPERATION) == MOV_OPERATION)
+        {
+          strcpy(operation, "mov");
+        }
+
+      printf(" to op: %s\n", operation);
+
     }
 
-  printf("\n");
-
   fclose(fp);
+
   return 0;
 }
