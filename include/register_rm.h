92652ec7e90491502350eb813e16db0671c6290c
mayermichael1 <michael.mayer2105@gmail.com>
2024-08-11 17:19:27 +0200

    mov out read and write value

--- include/register_rm.h
+++ include/register_rm.h
@@ -2,6 +2,7 @@
 #define REGISTER_RM_H
 
 #include "types.h"
+#include "binary.h"
 
 static const char* REGISTER_NAMES[] = 
 {
@@ -83,6 +84,46 @@ get_register_name (byte reg, bool wide)
     }
 
   return REGISTER_NAMES[reg];
+}
+
+inline word
+read_value_from_register (byte *registers, operand reg)
+{
+  word value = 0;
+  if (reg.type == OP_REGISTER)
+    {
+      if (reg.wide)
+        {
+          byte offset = REGISTER_OFFSET_WIDE[reg.reg];
+          value = *(word*)(registers+offset);
+        }
+      else
+        {
+          byte offset = REGISTER_OFFSET[reg.reg];
+          value = *(registers+offset);
+        }
+    }
+  return value;
+}
+
+inline void 
+write_value_to_register (byte *registers, operand reg, word value)
+{
+  if (reg.type == OP_REGISTER)
+    {
+      byte offset = REGISTER_OFFSET[reg.reg];
+
+      byte lowValue = get_low_byte(value);
+      byte highValue = get_high_byte(value);
+
+      if (reg.wide)
+        {
+          offset = REGISTER_OFFSET_WIDE[reg.reg];
+          registers[offset+1] = highValue;
+        }
+
+      registers[offset] = lowValue;
+    }
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,46 +14,6 @@
 byte registers[8*2] = {0};
 byte memory[MiB] = {0};
 
-inline word
-read_value_from_register (byte *registers, operand reg)
-{
-  word value = 0;
-  if (reg.type == OP_REGISTER)
-    {
-      if (reg.wide)
-        {
-          byte offset = REGISTER_OFFSET_WIDE[reg.reg];
-          value = *(word*)(registers+offset);
-        }
-      else
-        {
-          byte offset = REGISTER_OFFSET[reg.reg];
-          value = *(registers+offset);
-        }
-    }
-  return value;
-}
-
-inline void 
-write_value_to_register (byte *registers, operand reg, word value)
-{
-  if (reg.type == OP_REGISTER)
-    {
-      byte offset = REGISTER_OFFSET[reg.reg];
-
-      byte lowValue = get_low_byte(value);
-      byte highValue = get_high_byte(value);
-
-      if (reg.wide)
-        {
-          offset = REGISTER_OFFSET_WIDE[reg.reg];
-          registers[offset+1] = highValue;
-        }
-
-      registers[offset] = lowValue;
-    }
-}
-
 void 
 simulate_mov(operand destination, operand source)
 {
